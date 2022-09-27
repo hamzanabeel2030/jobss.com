218501f400196c7317d26c72c0265e4941a1fb97
hamzanabeel2030 <114527279+hamzanabeel2030@users.noreply.github.com>
2022-09-27 13:22:44 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+		<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+		<title>Royal Human Resources</title>
+	</head>
+<body>
+	<div class="top-bar">
+		<span><ion-icon name="logo-whatsapp"></ion-icon> 03355831248</span>
+		<ul>
+			<li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+			<li><a href="#"><ion-icon name="logo-twitter"></ion-icon></a></li>
+			<li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+		</ul>
+	</div>
+	<nav class="navbar">
+		<div class="logo">
+			<a href="#"><img src="images/RHC.png"></a>
+		</div>
+		<div class="toggle">
+			<a href="#"><ion-icon name="menu-outline"></ion-icon></a>
+		</div>
+		<ul class="menu">
+			<li><a href="jobs.php">Jobs</a></li>
+			<li><a href="#">Job Seeker</a></li>
+			<li><a href="#">Employer</a></li>
+			<li><a href="#">Contact Us</a></li>
+			<li><a href="#">Login</a></li>
+		</ul>
+	</nav>
+<!--header -->
+<header>
+	<h1 class="header-title">Find Your <span>Exciting Jobs</span> <br> And <span>Career Opportunities</span></h1>
+</header>
+<!-- search -->
+<div class="search-wrapper">
+	<div class="search-box">
+		<div class="search-card">
+			<input type="text" name="" class="search-input" placeholder="Enter Job Title">
+			<button class="search-button">Search</button>
+		</div>
+	</div>
+</div>
+<!-- end of search -->
+
+
+<!-- start of cvs section -->
+<section class="cvs" id="cvs">
+	<h1 class="section-title">The Collection Of Featured CVs</h1>
+	<p>Lorem ipsum smit oy Lorem ipsum smit oyLorem ipsum smit oyLorem ipsum smit oy</p>
+	<div class="featured-wrapper">
+		<div class="featured-card">
+			<img class="featured-image" src="images/growth.png">
+			<p>Marketing CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/motor.png">
+			<p>Electrical Engineering CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/production.png">
+			<p>Production Engineer CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/meeting.png">
+			<p>HR CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/software-development.png">
+			<p>Software Development CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/care.png">
+			<p>HSE CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/food-service.png">
+			<p>Hospitality CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/graduated.png">
+			<p>Fresh Graduate CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/online-shop.png">
+			<p>FMCG Sector CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/balance.png">
+			<p>Law CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/save-money.png">
+			<p>Finance CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/database.png">
+			<p>Female Database CVs</p>
+			<button class="featured-button">View 2 CVs</button>
+		</div>
+	</div>
+	
+</section>
+
+<!--end of cvs section -->
+
+<!-- start of jobs section -->
+<section class="jobs" id="jobs">
+	<h1 class="section-title">Bank of Jobs</h1>
+	<p>Lorem ipsum smit oy Lorem ipsum smit oyLorem ipsum smit oyLorem ipsum smit oy</p>
+	<div class="featured-wrapper">
+		<div class="featured-card">
+			<img class="featured-image" src="images/growth.png">
+			<p>Marketing Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/motor.png">
+			<p>Electrical Engineering Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/production.png">
+			<p>Production Engineer Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/meeting.png">
+			<p>HR Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/software-development.png">
+			<p>Software Development Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/care.png">
+			<p>HSE Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/food-service.png">
+			<p>Hospitality Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/graduated.png">
+			<p>Fresh Graduate Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/online-shop.png">
+			<p>FMCG Sector Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/balance.png">
+			<p>Law Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/save-money.png">
+			<p>Finance Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+		<div class="featured-card">
+			<img class="featured-image" src="images/database.png">
+			<p>Female Database Jobs</p>
+			<button class="featured-button">View 2 Jobs</button>
+		</div>
+	</div>
+	
+</section>
+
+<!-- end of job section -->
+
+
+<!-- footer start -->
+<footer>
+	<div class="footer-wrapper">
+		<h3>Royal Human Resources</h3>
+		<p>Lorem ipsem dollar sit Lorem ipsem dollar sit Lorem ipsem dollar sit Lorem ipsem dollar sit.</p>
+		<div class="social-media">
+			<a href="#"><i class="fab fa-facebook-f"></i></a>
+			<a href="#"><i class="fab fa-twitter"></i></a>
+			<a href="#"><i class="fab fa-linkedin-in"></i></a>
+			<a href="#"><i class="fab fa-youtube"></i></a>
+		</div>
+	</div>
+	<div class="footer-wrapper">
+		<h4>Explore</h4>
+		<a href="#">Top Companies</a>
+		<a href="#">Terms Of Services</a>
+		<a href="#">Podcast</a>
+		<a href="#">Careers</a>
+	</div>
+	<div class="footer-wrapper">
+		<h4>About</h4>
+		<a href="#">FAQs</a>
+		<a href="#">Get Inspired</a>
+		<a href="#">Blog</a>
+	</div>
+	<div class="footer-wrapper">
+		<h4>Support</h4>
+		<a href="#">Customer Service</a>
+		<a href="#">Trust & Safety</a>
+		<a href="#">Partnership</a>
+	</div>
+	<div class="footer-wrapper">
+		<h4>Community</h4>
+		<a href="#">Community</a>
+		<a href="#">Invite A Friend</a>
+		<a href="#">Event</a>
+	</div>
+</footer>
+
+
+
+ <!-- footer end -->
+
+
+
+<!-- javascript code for toggle button -->
+<script	src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script>
+	$(function(){
+		$(".toggle").on("click",function(){
+			if($(".menu").hasClass("active")){
+				$(".menu").removeClass("active");
+				$(this).find("a").html("<ion-icon name='menu-outline'></ion-icon>");
+			}else{
+				$(".menu").addClass("active");
+				$(this).find("a").html("<ion-icon name='close-outline'></ion-icon>");
+			}
+		});
+});
+
+</script>
+
+</body>
+</html>
---
+++ jobs.php
@@ -0,0 +1,60 @@
+<!-- job-listing -->
+<section class="job-list" id="jobs">
+	<div class="job-card">
+		<div class="job-name">
+			<img class="job-profile" src="images/growth.png">
+			<div class="job-detail">
+				<h4>Company Name</h4>
+				<h3>Mechanical Engineer</h3>
+				<p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+			</div>
+		</div>
+		<div class="job-label">
+			<a class="label-a" href="#">HTML</a>
+			<a class="label-b" href="#">CSS</a>
+			<a class="label-b" href="#">Javascript</a>
+		</div>
+		<div class="job-posted">
+			Posted 2 mint ago
+		</div>
+	</div>
+		<div class="job-card">
+		<div class="job-name">
+			<img class="job-profile" src="images/growth.png">
+			<div class="job-detail">
+				<h4>Company Name</h4>
+				<h3>Mechanical Engineer</h3>
+				<p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+			</div>
+		</div>
+		<div class="job-label">
+			<a class="label-a" href="#">HTML</a>
+			<a class="label-b" href="#">CSS</a>
+			<a class="label-b" href="#">Javascript</a>
+		</div>
+		<div class="job-posted">
+			Posted 2 mint ago
+		</div>
+	</div>
+		<div class="job-card">
+		<div class="job-name">
+			<img class="job-profile" src="images/growth.png">
+			<div class="job-detail">
+				<h4>Company Name</h4>
+				<h3>Mechanical Engineer</h3>
+				<p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
+			</div>
+		</div>
+		<div class="job-label">
+			<a class="label-a" href="#">HTML</a>
+			<a class="label-b" href="#">CSS</a>
+			<a class="label-b" href="#">Javascript</a>
+		</div>
+		<div class="job-posted">
+			Posted 2 mint ago
+		</div>
+	</div>
+	<button class="job-more">More Jobs</button>
+</section>
+
+<!-- end of job-listing -->
